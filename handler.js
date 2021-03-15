c8a18a7eff4a956621154b2dfb7ba958d786c077
David Dunleavy <A00256068@student.ait.ie>
2021-03-15 02:36:28 +0000

    Added DynamoDB content to serverless.yml and created table and post method for User

--- handler.js
+++ handler.js
@@ -9,6 +9,23 @@ const { v4: uuidv4 } = require('uuid');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.post("/users", async (req, res) => {
+  const data = req.body;
+  const params = {
+    TableName: "usersTable",
+    Item: {
+      id: uuidv4(),
+      name: data.name,
+      building_id: [1,2,3,4,5]
+    },
+  };
 
+  try {
+    await db.put(params).promise();
+    res.status(201).json({ user: params.Item });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 module.exports.app = serverless(app);
