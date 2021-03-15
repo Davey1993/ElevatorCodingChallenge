7b79be177d9a38eb3e18663ccdae24a24aaf9751
David Dunleavy <A00256068@student.ait.ie>
2021-03-15 02:24:08 +0000

    Removed unwanted content and updated serverless.yml & handler.js

--- handler.js
+++ handler.js
@@ -1,18 +1,14 @@
-'use strict';
+const serverless = require('serverless-http');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require('uuid');
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+
+
+module.exports.app = serverless(app);
