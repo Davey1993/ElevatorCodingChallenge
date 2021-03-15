5674e4277dbcc0aab0cd4d1f9a171f70cab252cc
David Dunleavy <A00256068@student.ait.ie>
2021-03-15 22:17:03 +0000

    Updating serverless.yml & handler.js with comments

--- handler.js
+++ handler.js
@@ -1,12 +1,12 @@
 /////////Imports///////////////////////////////
 
 const serverless = require('serverless-http');
-const express = require("express");
+const express = require("express"); 
 const app = express();
 const bodyParser = require("body-parser");
 const AWS = require("aws-sdk");
 const db = new AWS.DynamoDB.DocumentClient();
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid'); // I am using uuidv4 for creating id's for our users, buildings & elevators
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
